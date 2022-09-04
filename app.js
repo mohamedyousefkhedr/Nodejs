c1ad07a033ea92d91f3b9bc3b488997d4a56e606
moj93 <mohamedyousef.se@gmail.com>
2022-09-04 06:41:07 +0200

    updated

--- app.js
+++ app.js
@@ -3,29 +3,16 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
-//for mongodb
-const mongoose = require('mongoose');
-
-mongoose.connect(
-  "mongodb+srv://mojo:mojo@cluster0.091b6tu.mongodb.net/all-data?retryWrites=true&w=majority"
-)
-  .then(result => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+const Article = require("./models/articleSchema");
 
 // for auto refresh
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
-
+liveReloadServer.watch(path.join(__dirname, "public"));
 
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
@@ -36,43 +23,73 @@ liveReloadServer.server.once("connection", () => {
   }, 100);
 });
 
+// mongoose
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(
+    "mongodb+srv://mojo:mojo@cluster0.091b6tu.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
 
-
-
-
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.redirect("/all-articles");
-
 });
-
-
-
 
 app.get("/all-articles", (req, res) => {
-  res.render("index")
+  // res.render("index", { mytitle: "HOME" });
+
+  // result = Array of objects inside mongo database
+
+  Article.find()
+    .then((result) => {
+      res.render("index", { mytitle: "HOME", arrArticle: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+
+
+
+
+
+
+
+
+
+
 
 
 
 app.get("/add-new-article", (req, res) => {
-  res.render("add-new-article")
+  res.render("add-new-article", { mytitle: "create new article" });
 });
 
+app.post("/all-articles", (req, res) => {
+  const article = new Article(req.body);
 
+  // console.log(req.body)
 
+  article
+    .save()
+    .then((result) => {
+      res.redirect("/all-articles");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-//  404 
+//  404
 app.use((req, res) => {
   res.status(404).send("Sorry can't find that!");
-});
-
-
-
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
